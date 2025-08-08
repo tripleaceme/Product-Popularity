c7f6f6994f103c3ea2d1fef4cff728b6d69428f4
Adegbite Ayoade <tripleaceme@gmail.com>
2025-08-08 22:24:26 +0100

    seeting up python scripts

---
+++ etl_script.py
@@ -0,0 +1,6 @@
+# The transformation carried here doesn't impact business logic.
+# It's to ensure that the ETL script runs without errors and to maintain the integrity of the data pipeline.
+# It ensure data are processed correctly and efficiently.
+
+
+
